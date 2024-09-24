68dbead8644d77102fe31160415df757efd6c568
Roshan B Varghese <roshanbvarghese361@gmail.com>
2024-09-24 10:13:26 +0530

    Sample_Project

---
+++ SampleProject/src/main/java/sample/Sample_Project.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Sample_Project {
+
+	int x;
+	String y;
+	public void name(int a, String b) {
+		x = a;
+		y = b;
+		System.out.println(x+" "+y);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sample_Project A = new Sample_Project();
+		A.name(14, "Roshan");
+	}
+
+}
